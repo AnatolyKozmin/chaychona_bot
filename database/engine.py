559ae70f4b4c1f4a48419d8719ea607bff445744
«тупая <anatolii.kozmin@inbox.ru>
2025-07-18 14:22:48 +0300

    Update engine

--- database/engine.py
+++ database/engine.py
@@ -2,15 +2,28 @@ import os
 from dotenv import load_dotenv, find_dotenv 
 load_dotenv(find_dotenv())
 
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncAttrs
+from datetime import datetime
+from sqlalchemy import Integer, func
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+from config import settings
 
 
-DB_URL = os.getenv("DB_URL")
+DB_URL = settings.get_db_url()
 
-engine = create_async_engine(DB_URL, echo=True)
-async_session = sessionmaker(engine, expire_on_commit=True, class_=AsyncSession)
+engine = create_async_engine(url=DB_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=True)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower() + 's'
